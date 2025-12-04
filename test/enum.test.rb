22ee8c0046ed432ca6a09d79ec74bcd553f3a89a
Stephen Ierodiaconou <stevegeek@gmail.com>
2025-12-05 00:59:34 +0100

    Enum __after_defined__ still using old TypeError class method

--- test/enum.test.rb
+++ test/enum.test.rb
@@ -227,3 +227,17 @@ test "pattern matching" do
 	match { Color::Red => Color[1] }
 	match { Color::Red => Color[hex: "#FF0000"] }
 end
+
+test "index with wrong type raises Literal::TypeError" do
+	assert_raises(Literal::TypeError) do
+		Class.new(Literal::Enum(Integer)) do
+			index :bad_index, String do |member|
+				member.value # returns Integer, not String
+			end
+
+			A = new(1)
+
+			__after_defined__
+		end
+	end
+end
